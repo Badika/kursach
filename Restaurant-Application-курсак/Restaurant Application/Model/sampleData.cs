97e511aded1c363b56035ba26fbb64b0f5a7a0ab
Badika <victoria.figurska0@gmail.com>
2018-06-02 14:31:54 +0300

    1

---
+++ Restaurant-Application-курсак/Restaurant Application/Model/sampleData.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_Application.Model
+{
+    public class sampleData : DropCreateDatabaseAlways<RestaurantDB>
+    {
+        enum bookingstatus { Available, Reserved, Booked }
+        protected override void Seed(RestaurantDB context)
+        {
+            try
+            {
+                List<FoodItems> fooditems = new List<FoodItems>()
+                {
+                    new FoodItems { FoodName = "Суші", Description = "Авокадо, сир, рис, васабі, імбир", fPrice = 25 },
+                    new FoodItems { FoodName = "Борщ", Description = "морква, буряк, вода", fPrice = 25 },
+                    new FoodItems { FoodName = "Морозиво", Description = "молоко, цукор", fPrice = 18 },
+                    new FoodItems { FoodName = "Канапки", Description = "хліб, масло, сир", fPrice = 12 },
+                    new FoodItems { FoodName = "Шаурма", Description = "мясо", fPrice = 5 }
+                };
+                foreach(FoodItems f in fooditems)
+                {
+                    context.FoodItems.Add(f);
+                }
+                List<TableList> tablelist = new List<TableList>()
+                {
+                    new TableList { TableName = "Столик 1", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList { TableName = "Столик 2", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList { TableName = "Столик 3", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList { TableName = "Столик 4", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList { TableName = "Столик 5", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList { TableName = "Столик 6", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList { TableName = "Столик 7", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList { TableName = "Столик 8", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList { TableName = "Столик 9", BookingStatus = bookingstatus.Available.ToString() },
+                    new TableList { TableName = "Столик 10", BookingStatus = bookingstatus.Available.ToString() }
+                };
+                foreach (TableList t in tablelist)
+                {
+                    context.TableList.Add(t);
+                }
+                context.SaveChanges();
+                base.Seed(context);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ Restaurant-Application-курсак/Restaurant Application/Page Screens/AddFoodItems.xaml.cs
@@ -0,0 +1,73 @@
+﻿using Restaurant_Application.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Restaurant_Application.DB_Layer;
+using Restaurant_Application.ViewModel;
+
+namespace Restaurant_Application.Page_Screens
+{
+    /// <summary>
+    /// Interaction logic for AddFoodItems.xaml
+    /// </summary>
+    public partial class AddFoodItem : Page
+    {
+        private RestaurantViewModel _rVmObj;
+        public AddFoodItem()
+        {
+            InitializeComponent();
+            this.WindowHeight = 450;
+            this.WindowWidth = 600;
+            DataContext = new RestaurantViewModel();
+        }
+
+        public void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            foodnametxt.Clear();
+            Descriptiontxt.Clear();
+            pricetxt.Clear();
+        }
+
+        public void AddItem_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (foodnametxt.Text == "" || Descriptiontxt.Text == "" || pricetxt.Text == "")
+                {
+                    status.Foreground = Brushes.Red;
+                    status.Content = "Усі поля обов'язкові.";
+                }
+                else
+                {
+                    _rVmObj = new RestaurantViewModel();
+                    FoodItems fooditem = new FoodItems();
+                    fooditem.FoodName = foodnametxt.Text;
+                    fooditem.Description = Descriptiontxt.Text;
+                    fooditem.fPrice = Convert.ToInt32(pricetxt.Text.ToString());
+                    _rVmObj.AddFoodItem(fooditem);
+                    DataContext = new RestaurantViewModel();
+                    status.Foreground = Brushes.Green;
+                    status.Content = "Страва успішно додана.";
+                }
+            }
+            catch (Exception exp)
+            {
+                status.Foreground = Brushes.Red;
+                status.Content = "Введіть значення.";
+            }
+
+        }
+
+    }
+}
